9e953bcff48d4572d94f72c659b6fc2bb9379ab7
pitsolu <pitsolu@gmail.com>
2020-10-15 15:17:30 +0300

    medium sized refactor

--- package/lib/App/Package/PkgTests.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace App\Package;
-
-class PkgTests{}
---
+++ src/Strukt/Package/PkgTests.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Strukt\Package;
+
+use Strukt\Contract\Package as Pkg;
+
+class PkgTests implements Pkg{
+
+	private $manifest;
+
+	public function __construct(){
+
+		$this->manifest = array(
+
+			"package"=>"pkg-tests",
+			"files"=>array(
+
+				"lib/App/Command/Tests/Exec.php",
+				"lib/App/Command/Tests/ListCmd.php"
+			)
+		);
+	}
+
+	public function getName(){
+
+		return $this->manifest["package"];
+	}
+
+	public function getFiles(){
+
+		return $this->manifest["files"];
+	}
+
+	public function getModules(){
+
+		return null;
+	}
+}
